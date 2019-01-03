ad5b055de874bede8dc23cc17329c29b5357a56d
afzal63 <42829235+afzal63@users.noreply.github.com>
2019-01-03 13:07:38 +0530

    Code files updated

---
+++ Azure-Mongo-DB/Dal.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyTaskListApp.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Configuration;
+using System.Security.Authentication;
+
+namespace MyTaskListApp
+{
+    public class Dal : IDisposable
+    {
+        //private MongoServer mongoServer = null;
+        private bool disposed = false;
+        // or IP address of your server. 
+        //For example, "mongodb://testlinux.cloudapp.net
+        private string userName = "cosmosmongodb1";
+        private string host = "cosmosmongodb1.documents.azure.com";
+        private string password = "NrIXm7oo3uWCiAL2ZT3CvvzRtt2DaBbplyj4FZZZphX11wxCmWWtr7wMZD1PDq6Y76ECqUNdPpELla2JSzIojw==";
+
+        // This sample uses a database named "Tasks" and a 
+        //collection named "TasksList".  The database and collection 
+        //will be automatically created if they don't already exist.
+        private string dbName = "db";
+        private string collectionName = "families";
+
+        // Default constructor.        
+        public Dal()
+        {
+        }
+
+        // Gets all Task items from the MongoDB server.        
+        public List<MyTask> GetAllTasks()
+        {
+            try
+            {
+                var collection = GetTasksCollection();
+                return collection.Find(new BsonDocument()).ToList();
+            }
+            catch (MongoConnectionException)
+            {
+                return new List<MyTask>();
+            }
+        }
+
+        // Creates a Task and inserts it into the collection in MongoDB.
+        public void CreateTask(MyTask task)
+        {
+            var collection = GetTasksCollectionForEdit();
+            try
+            {
+                collection.InsertOne(task);
+            }
+            catch (MongoCommandException ex)
+            {
+                string msg = ex.Message;
+            }
+        }
+
+        private IMongoCollection<MyTask> GetTasksCollection()
+        {
+            MongoClientSettings settings = new MongoClientSettings();
+            settings.Server = new MongoServerAddress(host, 10255);
+            settings.UseSsl = true;
+            settings.SslSettings = new SslSettings();
+            settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+            MongoIdentity identity = new MongoInternalIdentity(dbName, userName);
+            MongoIdentityEvidence evidence = new PasswordEvidence(password);
+
+            settings.Credential = new MongoCredential("SCRAM-SHA-1", identity, evidence);
+
+            MongoClient client = new MongoClient(settings);
+            var database = client.GetDatabase(dbName);
+            var todoTaskCollection = database.GetCollection<MyTask>(collectionName);
+            return todoTaskCollection;
+        }
+
+        private IMongoCollection<MyTask> GetTasksCollectionForEdit()
+        {
+            MongoClientSettings settings = new MongoClientSettings();
+            settings.Server = new MongoServerAddress(host, 10255);
+            settings.UseSsl = true;
+            settings.SslSettings = new SslSettings();
+            settings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+            MongoIdentity identity = new MongoInternalIdentity(dbName, userName);
+            MongoIdentityEvidence evidence = new PasswordEvidence(password);
+
+            settings.Credential = new MongoCredential("SCRAM-SHA-1", identity, evidence);
+            
+            MongoClient client = new MongoClient(settings);
+            var database = client.GetDatabase(dbName);
+            var todoTaskCollection = database.GetCollection<MyTask>(collectionName);
+            return todoTaskCollection;
+        }
+
+        # region IDisposable
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        # endregion
+    }
+}
