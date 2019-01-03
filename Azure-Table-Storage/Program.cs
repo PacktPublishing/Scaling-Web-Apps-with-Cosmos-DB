f62c84b93fc1af4ecdf23897828651215e9871a0
afzal63 <42829235+afzal63@users.noreply.github.com>
2019-01-03 12:56:50 +0530

    Code files updated

---
+++ Azure-Table-Storage/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace TableStorage
+{
+    using System;
+
+    public class Program
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Table Storage - Getting Started Samples\n");
+            Console.WriteLine();
+
+            BasicSamples basicSamples = new BasicSamples();
+            basicSamples.RunSamples().Wait();
+
+            AdvancedSamples advancedSamples = new AdvancedSamples();
+            advancedSamples.RunSamples().Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+
+                     
+    }
+}
